5f5c17ecbe085de848d4646c66e14323f6a48559
Кузырятский Александр <a.kuzyryatskiy@timeweb.ru>
2026-05-26 18:23:34 +0200

    app example repo

---
+++ README.md
@@ -0,0 +1,2 @@
+app-example-bun-solid
+Пример приложения Solid на Bun для тестов Timeweb Cloud App Platform.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main>
+      <h1>Timeweb Cloud + Solid = ❤️</h1>
+    </main>
+  )
+}
+
+export default App
